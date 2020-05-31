8348f5dc71347dc62959aa15d93474f9aedfc687
rattapon-kam <rattapon.k@outlook.com>
2020-05-31 16:08:28 +0700

    Excercise4

---
+++ Exercise4_Rattapon_K.py
@@ -0,0 +1,9 @@
+FE=12.5
+GB=75.2
+CS=85.9
+CP=97.6
+print("--- Your Score ---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",CS)
+print("Computer Programming :",CP)
