d1b54c33ba2dd9d9922d853aed33433a97ea544d
rattapon-kam <rattapon.k@outlook.com>
2020-06-01 22:11:29 +0700

    Create Exercise5_1_Rattapon_k.py

---
+++ Exercise5_1_Rattapon_k.py
@@ -0,0 +1,6 @@
+Num1=int(input("Please Enter Number1 : "))
+Num2=int(input("Please Enter Number2 : "))
+print(Num1,"+",Num2,"=",Num1+Num2)
+print(Num1,"-",Num2,"=",Num1-Num2)
+print(Num1,"*",Num2,"=",Num1*Num2)
+print(Num1,"/",Num2,"=",Num1/Num2)
